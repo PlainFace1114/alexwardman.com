d5f5c22c86ee233336afe68ffe3b2864df11bf19
Alex Wardman <69607939+PlainFace1114@users.noreply.github.com>
2025-02-07 22:53:22 -0600

    Update chess.js

--- chess.js
+++ chess.js
@@ -12,9 +12,21 @@ const MyButton = document.getElementById('MyButton');
     //Add on click listener for button
 MyButton.addEventListener('click', function() {
         //Select (h1) heading by id, and then change it's value to (bananas)
-    if (document.getElementById('MyHeading').innerText == "Qxd6 cxd6"){
+    document.getElementById('MyHeading').innerText == "Qxd6 cxd6"
+})
+
+const MyButton = document.getElementById('MyButton2');
+    //Add on click listener for button
+MyButton.addEventListener('click', function() {
+        //Select (h1) heading by id, and then change it's value to (bananas)
     document.getElementById('MyHeading').innerText = "Ba5 Qb6";
-    } else {
-        document.getElementById('MyHeading').innerText = "Bxb6#";
+
+})
+
+const MyButton = document.getElementById('MyButton3');
+    //Add on click listener for button
+MyButton.addEventListener('click', function() {
+        //Select (h1) heading by id, and then change it's value to (bananas)
+    document.getElementById('MyHeading').innerText = "Bxb6#";
     }
 })
