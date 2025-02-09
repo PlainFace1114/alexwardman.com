f0f2bdb132a0f0692d6243598b524c424850cd05
Alex Wardman <69607939+PlainFace1114@users.noreply.github.com>
2025-02-08 22:23:41 -0600

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,5 +1,5 @@
 //chess puzzles script
-const images = ["img/begin.png", "img/move1-w.png", "img/move1-b.png", "img/move2-w.png", "img/move2-b.png", "img/mate.png"];
+const images = ["img/begin.webp", "img/move1-w.webp", "img/move1-b.webp", "img/move2-w.webp", "img/move2-b.webp", "img/mate.webp"];
 const buttonTexts = ["White to Move", "Qxd6", "cxd6", "Ba5", "Qb6", "Bxb6#"];
 let currentIndex = 0;
 
